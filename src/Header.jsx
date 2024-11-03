84e1c1b01b887b4618d46681d0866e8440b1eaca
Hala <halachennai527@gmail.com>
2024-11-03 09:41:23 +0530

    hi da value

--- src/Header.jsx
+++ src/Header.jsx
@@ -40,7 +40,7 @@ function Header() {
     }
 
     try {
-        const response = await fetch('http://localhost:5000/start-stream', {
+        const response = await fetch(`${import.meta.env.VITE_URL}/start-stream`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ streamKey, roomId }),
